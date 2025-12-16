2f4376db1838e781f9e3a8ffa6884d27f4f42658
yaswanthkumar26 <amarapalliyaswanthkumar@gmail.com>
2025-12-16 13:38:57 +0530

    Clean up comments in App.js
    Removed unnecessary comments from App.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ import NotFound from './components/NotFound'
 import ProtectedRoute from './components/ProtectedRoute'
 import './App.css'
 
-// These are the lists used in the application. You can move them to any component needed.
-
-// Replace your code here
 
 const App = () => (
   <Switch>
